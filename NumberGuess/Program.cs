cb1d052038b4c715d2160e20ac18e66df8ed2aec
Jarrod Grech <ladopolo@Jarrods-MacBook-Pro.local>
2025-06-24 17:57:23 +1000

    Number Guess Game

---
+++ NumberGuess/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string? guess;
+        int randomNumber;
+        int guessedNumero;
+        bool gameRunning = true;
+
+        while (gameRunning)
+        {
+            Random random = new();
+            randomNumber = random.Next(1, 11);
+            Console.WriteLine("I am thinking of a number between 1 and 10");
+            guess = Console.ReadLine();
+            guessedNumero = int.Parse(guess!);
+
+            if (guessedNumero == randomNumber)
+            {
+                Console.WriteLine("Correct!");
+            }
+            else
+            {
+                Console.WriteLine("Wrong!");
+            }
+        }
+    }
+}
